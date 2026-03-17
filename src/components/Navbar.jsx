de251a398d95c0d93d3d364c85d2564f0abf2568
joeyfenix <joeyfenix@github.com>
2026-03-17 06:24:23 +0000

    implement hero scroll animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
         start: 'bottom top'
       }
     });
-    navTween.fromTo('nav', { backgroundColor: 'transparent'},{ 
-      backgroundColor: '#00000050',
-      backgroundFilter: 'blur (10px)', 
-      duration: 1,
-      ease: 'power1.inOut'
-    });
+    	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut'
+      });
   })
   return (
     <nav>
